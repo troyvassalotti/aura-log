1a1395c6407d3f9db7f2632c8cbb399dad97edb9
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2025-07-30 23:15:04 -0400

    Return merged config in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@
 
 import {dirname, resolve} from "node:path";
 import {fileURLToPath} from "node:url";
-import {defineConfig} from "vite";
+import {cwd} from "node:process";
+import {mergeConfig} from "vite";
 import generateFile from "vite-plugin-generate-file";
 import handlebars from "vite-plugin-handlebars";
 import config from "./src/lib/AuralogConfig.js";
@@ -11,40 +12,68 @@ import data from "./src/plugins/generate-files.js";
 /** Directory of this package. */
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-	build: {
-		rollupOptions: {
-			// Use auralog package index.html instead of process CWD
-			input: {
-				main: resolve(__dirname, "index.html"),
-			},
-			// Maintain output at process root instead of package (node_modules) root
-			output: {
-				dir: resolve(process.cwd(), "dist"),
+class ViteConfig {
+	static get cwd() {
+		return cwd();
+	}
+
+	static configFileName = "vite.config.js";
+
+	static get userConfigPath() {
+		return resolve(this.cwd(), this.configFileName);
+	}
+
+	static async fetchUserConfig() {
+		try {
+			const userConfig = await import(this.userConfigPath);
+			return userConfig.default;
+		} catch (error) {
+			console.error(error);
+			return {};
+		}
+	}
+
+	static defaultConfig = {
+		build: {
+			rollupOptions: {
+				// Use auralog package index.html instead of process CWD
+				input: {
+					main: resolve(__dirname, "index.html"),
+				},
+				// Maintain output at process root instead of package (node_modules) root
+				output: {
+					dir: resolve(cwd(), "dist"),
+				},
 			},
 		},
-	},
-	plugins: [
-		generateFile([
-			{
-				type: "json",
-				output: "./headaches.json",
-				data,
-			},
-		]),
-		handlebars({
-			context: {
-				title: config?.html?.title,
-				description: config?.html?.description,
-				meta: config?.html?.meta,
-				allowRobots: config?.allowRobots,
-			},
-		}),
-	],
-	preview: {
-		open: "/",
-	},
-	server: {
-		open: "/node_modules/@troyv/auralog/index.html",
-	},
-});
+		plugins: [
+			generateFile([
+				{
+					type: "json",
+					output: "./headaches.json",
+					data,
+				},
+			]),
+			handlebars({
+				context: {
+					title: config?.html?.title,
+					description: config?.html?.description,
+					meta: config?.html?.meta,
+					allowRobots: config?.allowRobots,
+				},
+			}),
+		],
+		preview: {
+			open: "/",
+		},
+		server: {
+			open: "/node_modules/@troyv/auralog/index.html",
+		},
+	};
+}
+
+export default mergeConfig(
+	ViteConfig.defaultConfig,
+	ViteConfig.fetchUserConfig(),
+	false,
+);
